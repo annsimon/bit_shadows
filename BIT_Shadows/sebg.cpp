3168ac80128f105f2c2e0196437cf61f00829da8
noreia <sbexten@web.de>
2014-06-13 20:57:32 +0200

    segb ohne bilder-debug

--- BIT_Shadows/sebg.cpp
+++ BIT_Shadows/sebg.cpp
@@ -30,9 +30,6 @@ bool Sebg::run(QStringList* originals)
         m_sebg.findSegmentation();
         saveResult(originals->at(i));
 
-        if(i == 5){
-            m_sebg.showSpecificFrame();
-        }
     }
 
     return true;
